44427d6a3385bd85f99a01afa5139d9aaa55e5db
Mert Akpınar <61903599+mertakpinar29@users.noreply.github.com>
2020-03-09 17:23:09 +0300

    Add files via upload

---
+++ testClass.java
@@ -0,0 +1,10 @@
+public class testClass {
+    public static void main(String[] args){
+        String[][] Map=new String[10][10];
+        main.Entrance();
+        main.displayEmptyMap(Map);
+        main.playerShips(Map);
+        main.computerShips(Map);
+        main.Battle(Map);
+    }
+}
